539bce9792ae7ef8d6db2ae5d90bb509538c901d
ryougifujino <ryougifujino@gmail.com>
2022-04-11 23:14:49 +0800

    release: cut the v0.19.0 release

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,7 +1,11 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [2, 'always', ['build', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test']],
+    'type-enum': [
+      2,
+      'always',
+      ['build', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test', 'release'],
+    ],
     'body-max-line-length': [0],
   },
 }
